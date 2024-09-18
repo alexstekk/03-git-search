c195b8f828fd343d8ddbc9d12e110eb753bf6f8b
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-09-18 11:15:04 +0300

    Search component with useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Container } from "components/Container";
+import { Search } from "components/Search";
 import { TheHeader } from "components/TheHeader";
 
 function App() {
   return (
     <Container>
       <TheHeader />
+      <Search
+        hasError
+        onSubmit={() => {}}
+      />
     </Container>
   );
 }
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,44 @@
+import { ReactComponent as SearchIcon } from "assets/icon-search.svg";
+
+import styles from "./Search.module.scss";
+import { Button } from "components/Button";
+import { useRef } from "react";
+
+interface SearchProps {
+  hasError: boolean;
+  onSubmit: (text: string) => void;
+}
+
+export const Search = ({ hasError, onSubmit }: SearchProps) => {
+  const searchRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const text = searchRef.current ? searchRef.current.value : "";
+
+    if (text) {
+      onSubmit(text);
+      if (searchRef.current) searchRef.current.value = "";
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.search}>
+        <label htmlFor="search">
+          <SearchIcon />
+        </label>
+        <input
+          ref={searchRef}
+          type="text"
+          placeholder="Search GitHub username..."
+          name="username"
+          id="search"
+          className={styles.textField}
+        />
+        <Button>Search</Button>
+        {hasError && <span className={styles.error}>No result</span>}
+      </div>
+    </form>
+  );
+};
